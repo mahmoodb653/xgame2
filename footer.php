485dedf1349ddf398da65ed41390619abd52483d
AMG <icyheart73@gmail.com>
2020-11-09 17:24:50 +0330

    latest steps

--- footer.php
+++ footer.php
@@ -2,60 +2,29 @@
     <div class="container">
         <div class="row footer-row">
             <div class="col-lg-4 footer__about">
-                <div>
-                    <img class="about__logo" src="<?=get_field('footer_logo','option')?>" alt="" />
-                    <span class="about__text"
-                    >لورم ایپسوم متن ساختگی با تولید سادگی نامفهوم از صنعت چاپ و با استفاده از طراحان
-                گرافیک است . چاپگر ها و متون بلکه روزنامه و مجله در ستون و سطر آنچنان که لازم است و
-                برای شرایط فعلی تکنولوژی مورد نیاز و کاربرد های متنوع با هدف بهبود ابزار های کار
-                بردی می باشد. کتاب های زیادی در شصت و سه درصد گذشته، حال و آینده شناخت فراوان جامعه
-                و متخصصان را می طلبد. <a href="#">مشاهده بیشتر</a></span
-                    >
-                </div>
+                <?php if ( is_active_sidebar( 'primary_widget_area' ) ) : ?>
 
-                <span class="copyright"
-                >تمام حقوق این وب سایت متعلق به <a href="./index.html">ایکس گیمز</a> است.</span
-                >
+	            <?php dynamic_sidebar( 'primary_widget_area' ); ?>
+
+                <?php endif; ?>
+
             </div>
             <div class="col-lg-4 footer__tags">
-                <div class="headline footer__headline">
-                    <div class="headline-title">
-                        <i class="icon-tag headline-title__icon"></i>
-                        <span class="headline-title__text">تگ های محبوب</span>
-                    </div>
-                </div>
-                <div class="tags-container">
-                    <a href="#" class="tag">ارزان</a>
-                    <a href="#" class="tag">خرید سی دی کی</a>
-                    <a href="#" class="tag">CDKEY</a>
-                    <a href="#" class="tag">ONLINE</a>
-                    <a href="#" class="tag">SHOP</a>
-                    <a href="#" class="tag">GAME</a>
-                    <a href="#" class="tag">بازی</a>
-                    <a href="#" class="tag">آنلاین</a>
-                    <a href="#" class="tag">فورتنایت</a>
-                    <a href="#" class="tag">مورتال کامبت</a>
-                </div>
+	            <?php if ( is_active_sidebar( 'secondary_widget_area' ) ) : ?>
+
+		            <?php dynamic_sidebar( 'secondary_widget_area' ); ?>
+
+	            <?php endif; ?>
+
             </div>
             <div class="col-lg-4 footer__external-link">
-                <div class="headline footer__headline">
-                    <div class="headline-title">
-                        <i class="icon-globe headline-title__icon"></i>
-                        <span class="headline-title__text">شبکه های اجتماعی</span>
-                    </div>
-                </div>
-                <div class="social-container">
-                    <a href="#" class="social">تلگرام <i class="icon-telegram"></i></a>
-                    <a href="#" class="social">یوتیوب <i class="icon-youtube"></i></a>
-                    <a href="#" class="social">اینستاگرام <i class="icon-instagram"></i></a>
-                    <a href="#" class="social">آپارات <i class="icon-aparat"></i></a>
-                </div>
 
-                <div class="license-container">
-                    <div class="license"></div>
-                    <div class="license"></div>
-                    <div class="license"></div>
-                </div>
+	            <?php if ( is_active_sidebar( 'third_widget_area' ) ) : ?>
+
+		            <?php dynamic_sidebar( 'third_widget_area' ); ?>
+
+	            <?php endif; ?>
+
             </div>
         </div>
     </div>
